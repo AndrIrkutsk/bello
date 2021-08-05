be1a9763820b46ed69776c37b198b95f3dd6b1ba
AndrIrkutsk <57530249+AndrIrkutsk@users.noreply.github.com>
2021-08-05 19:44:32 +0800

    Update modal2.js

--- js/modal2.js
+++ js/modal2.js
@@ -20,4 +20,38 @@ $(document).ready(function(){
     }
     $(this).toggleClass('active').next().slideToggle(300);
   });
+
+
+  /* ------------------------------ */
+  /* scroll jQuery -- js scroll or jQuery scroll */
+  $('a[data-scroll]').on("click", function (e) {
+    var anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top - 50
+    }, 1000);
+    e.preventDefault();
+  });
+  
+  // Navigation Close On Click Function
+  $(document).on('click', '.navbar-collapse.in', function (e) {
+    if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+      $(this).collapse('hide');
+    }
+  });
+  
+  $(window).on("scroll", function(){
+    /*
+    * ----------------------------------------------------------------------------------------
+    *   header On Scroll Animation
+    * ----------------------------------------------------------------------------------------
+    */  
+   
+   var header = $(".header");
+   if (header.offset().top > 50) {
+     header.addClass("nav-collapsed");
+    } else {
+      header.removeClass("nav-collapsed");
+    }        
+  });
+  /* ------------------------------ */
 });
